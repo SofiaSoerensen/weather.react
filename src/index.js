97037e17c2ac04d4d354f55519a07eac7ca364ba
Sofia Soerensen <sofia191004@gmail.com>
2025-03-26 16:01:12 +0100

    Added footer with link to repository

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Search from "./Search";
+import Footer from "./Footer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
     <Search />
+    <Footer />
   </React.StrictMode>
 );
 
