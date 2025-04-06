c20846cc405ca840ec7fe56866e7f243843e59e8
Sofia Soerensen <sofia191004@gmail.com>
2025-04-06 11:06:43 +0200

    Added HTML

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React from "react";
 import "./App.css";
-import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by Sofia Soerensen,{" "}
+          <a href="https://github.com/SofiaSoerensen/weather.react">
+            is open-sourced on Github
+          </a>{" "}
+          and hosted on Netlify
+        </footer>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Search from "./Search";
-import Footer from "./Footer";
+import "bootstrap/dist/css/bootstrap.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    <Search />
-    <Footer />
   </React.StrictMode>
 );
 
